621600df1cabc3e7eebe437e8b525152b023b5c6
hjkim0905 <eax9952@gmail.com>
2024-06-22 16:12:51 +0900

    240622 : 프로젝트 최초 생성

---
+++ mumuk/Pods/Target Support Files/Pods-mumukTests/Pods-mumukTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mumukTests : NSObject
+@end
+@implementation PodsDummy_Pods_mumukTests
+@end
---
+++ mumuk/mumuk/ViewController.swift
@@ -0,0 +1,38 @@
+//ViewController.swift
+
+import CoreLocation
+import NMapsMap
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    var locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let mapView = NMFMapView(frame: view.frame)
+        view.addSubview(mapView)
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestWhenInUseAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            print("위치 서비스 On 상태")
+            locationManager.startUpdatingLocation()
+            print(locationManager.location?.coordinate)
+            
+            //현 위치로 카메라 이동
+            let cameraUpdate = NMFCameraUpdate(scrollTo: NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0))
+            cameraUpdate.animation = .easeIn
+            mapView.moveCamera(cameraUpdate)
+            
+            let marker = NMFMarker()
+            marker.position = NMGLatLng(lat: locationManager.location?.coordinate.latitude ?? 0, lng: locationManager.location?.coordinate.longitude ?? 0)
+            marker.mapView = mapView
+        } else {
+            print("위치 서비스 Off 상태")
+        }
+
+    }
+}
