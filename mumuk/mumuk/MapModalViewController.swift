62aedc4ca1e12dce9f6e1f7790779a1313700233
minjun kim <162880508+minzzzun@users.noreply.github.com>
2024-06-26 22:03:42 +0900

    민준: 지도 커스텀 기능 추가(진행중)

---
+++ mumuk/mumuk/MapModalViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapModalView.swift
+//  mumuk
+//
+//  Created by 김민준 on 6/26/24.
+//
+
+import UIKit
+
+class MapModalViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+    
+    
+    
+    
+}
+
+
+
+
+
